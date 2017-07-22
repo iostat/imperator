e3c19cb408444aee235498d9387dcb499c40236a
Ilya Ostrovskiy <iostat@users.noreply.github.com>
2017-07-22 16:49:55 -0400

    add gratuitous AST output, because we can

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ import           Control.Monad
 import           System.Environment    (getArgs)
 import           System.IO             (BufferMode (NoBuffering), hSetBuffering,
                                         stdin, stdout)
+import           Text.Show.Prettyprint
 
 import           Imperator.Interpreter
 import           Imperator.Parser
@@ -21,6 +22,11 @@ main = do
   file <- readFile fileName
   case parseImperator fileName file of
     Left parseError -> error $ "Error when parsing: " ++ show parseError
-    Right stmt      -> runImperator stmt >>= \case
-      Left interpError -> error $ "Error while interpreting: " ++ show interpError
-      Right res        -> return ()
+    Right stmt      -> do
+      putStrLn "===== gratuitous AST output ====="
+      prettyPrint stmt
+      putStrLn "================================="
+      putStrLn ""
+      runImperator stmt >>= \case
+        Left interpError -> error $ "Error while interpreting: " ++ show interpError
+        Right res        -> return ()
